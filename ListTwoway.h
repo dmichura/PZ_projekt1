e3031b483bdb739ef43d36e840071df0b4d520e5
dmichura <dejwidcode@gmail.com>
2025-10-26 02:57:25 +0200

    Added comments for doxygen & split file

--- ListTwoway.h
+++ ListTwoway.h
@@ -1,232 +1,109 @@
+/**
+ * @file ListTwoway.h
+ * @brief Definicja klasy szablonowej ListTwoway — implementacja listy dwukierunkowej w C++.
+ * 
+ * Klasa ListTwoway umożliwia dodawanie, usuwanie, wstawianie oraz czyszczenie elementów listy.
+ * Zawiera wskaźniki do pierwszego i ostatniego elementu listy oraz zmienną przechowującą jej rozmiar.
+ * 
+ * @tparam Type Typ danych przechowywanych w liście.
+ * @author dmichura
+ * @date 2025-10-25
+ */
+
 #ifndef LIST_TWOWAY_H
 #define LIST_TWOWAY_H
 
 #include "Item.h"
 
+/**
+ * @class ListTwoway
+ * @brief Klasa szablonowa reprezentująca listę dwukierunkową.
+ * 
+ * Klasa przechowuje elementy typu `Item<Type>` połączone wskaźnikami w obu kierunkach.
+ * Umożliwia dodawanie elementów na początek i koniec listy, wstawianie elementów w dowolne miejsce,
+ * usuwanie elementów oraz czyszczenie całej listy.
+ *
+ * @tparam Type Typ danych przechowywanych w liście.
+ */
 template <typename Type>
 class ListTwoway {
-    private:
-        Item<Type>* head;
-        Item<Type>* tail;
-        int size=0;
-    public:
-        ListTwoway();
-        ~ListTwoway();
+private:
+    Item<Type>* head;   ///< Wskaźnik na pierwszy element listy.
+    Item<Type>* tail;   ///< Wskaźnik na ostatni element listy.
+    int size = 0;       ///< Aktualny rozmiar listy (liczba elementów).
 
-        void unshift(Type data);
-        void push(Type data);
-        void insertAt(int index, Type data);
-        void removeAt(int index);
-        void shift();
-        void pop();
-        void clear();
+public:
+    /**
+     * @brief Konstruktor domyślny klasy ListTwoway.
+     * 
+     * Tworzy pustą listę, ustawiając wskaźniki `head` i `tail` na `nullptr`.
+     */
+    ListTwoway();
 
-        int getSize();
-        Item<Type>* getHead();
-        Item<Type>* getTail();
+    /**
+     * @brief Destruktor klasy ListTwoway.
+     * 
+     * W razie potrzeby może zostać rozszerzony o automatyczne czyszczenie listy.
+     */
+    ~ListTwoway();
+
+    /**
+     * @brief Dodaje nowy element na początek listy.
+     * @param data Wartość elementu do dodania.
+     */
+    void unshift(Type data);
+
+    /**
+     * @brief Dodaje nowy element na koniec listy.
+     * @param data Wartość elementu do dodania.
+     */
+    void push(Type data);
+
+    /**
+     * @brief Wstawia element w określonym miejscu w liście.
+     * @param index Indeks miejsca, w którym element ma zostać wstawiony.
+     * @param data Wartość elementu do wstawienia.
+     */
+    void insertAt(int index, Type data);
+
+    /**
+     * @brief Usuwa element z określonego miejsca w liście.
+     * @param index Indeks elementu do usunięcia.
+     */
+    void removeAt(int index);
+
+    /**
+     * @brief Usuwa pierwszy element listy.
+     */
+    void shift();
+
+    /**
+     * @brief Usuwa ostatni element listy.
+     */
+    void pop();
+
+    /**
+     * @brief Usuwa wszystkie elementy z listy i resetuje jej rozmiar.
+     */
+    void clear();
+
+    /**
+     * @brief Zwraca aktualną liczbę elementów listy.
+     * @return Liczba elementów w liście.
+     */
+    int getSize();
+
+    /**
+     * @brief Zwraca wskaźnik na pierwszy element listy.
+     * @return Wskaźnik na głowę listy.
+     */
+    Item<Type>* getHead();
+
+    /**
+     * @brief Zwraca wskaźnik na ostatni element listy.
+     * @return Wskaźnik na ogon listy.
+     */
+    Item<Type>* getTail();
 };
 
-
-template<typename Type>
-int ListTwoway<Type>::getSize()
-{
-    return size;
-}
-
-template<typename Type>
-Item<Type>* ListTwoway<Type>::getHead()
-{
-    return head;
-}
-template<typename Type>
-Item<Type>* ListTwoway<Type>::getTail()
-{
-    return tail;
-}
-
-template<typename Type>
-ListTwoway<Type>::ListTwoway()
-{
-    head = nullptr;
-    tail = nullptr;
-}
-
-template<typename Type>
-ListTwoway<Type>::~ListTwoway()
-{
-    
-}
-
-template<typename Type>
-void ListTwoway<Type>::unshift(Type data)
-{
-    Item<Type>* newItem = new Item<Type>(data);
-
-    if(!head)
-    {
-        head = tail = newItem;
-    }
-    else
-    {
-        newItem->next = head;
-        head->prev = newItem;
-        head = newItem;
-    }
-    size++;
-}
-template<typename Type>
-void ListTwoway<Type>::push(Type data)
-{
-    Item<Type>* newItem = new Item<Type>(data);
-
-    if(!head)
-    {
-        head = tail = newItem;
-    }
-    else
-    {
-        tail->next = newItem;
-        newItem->prev = tail;
-        tail = newItem;
-    }
-    size++;
-}
-
-template<typename Type>
-void ListTwoway<Type>::insertAt(int index, Type data)
-{
-    if (index < 0 || index > size) {
-        cout << "Index jest niepoprawny!" << endl << "Index musi byc >= 0 i nie moze byc > size!" << endl;
-        return;
-    }
-
-    if (index == 0) {
-        unshift(data);
-        return;
-    }
-
-    if (index == size) {
-        push(data);
-        return;
-    }
-
-    Item<Type>* newItem = new Item<Type>(data);
-    Item<Type>* current = head;
-
-    for (int i = 0; i < index - 1; ++i) {
-        current = current->next;
-
-    }
-
-    newItem->next = current->next;
-    if (current->next) {
-        current->next->prev = newItem;
-    }
-
-    current->next = newItem;
-    newItem->prev = current;
-
-    size++;
-}
-
-template<typename Type>
-void ListTwoway<Type>::removeAt(int index)
-{
-    if (index < 0 || index >= size) {
-        cout << "Nieprawidłowy indeks!" << endl;
-        return;
-    }
-
-
-    if (index == 0) {
-        shift();
-        return;
-    }
-
-
-    if (index == size - 1) {
-        pop();
-        return;
-    }
-
-
-    Item<Type>* current = head;
-    for (int i = 0; i < index; ++i) {
-        current = current->next;
-    }
-
-    current->prev->next = current->next;
-    if (current->next) {
-        current->next->prev = current->prev;
-    }
-
-    delete current;
-
-    size--;
-}
-
-template<typename Type>
-void ListTwoway<Type>::shift()
-{
-
-    if (!head) {
-        cout << "Lista jest pusta!" << endl;
-        return;
-    }
-
-
-    if (head == tail) {
-        delete head;
-        head = tail = nullptr;
-    }
-    else {
-        Item<Type>* temp = head;
-        head = head->next;
-        head->prev = nullptr;
-        delete temp;
-    }
-
-
-    size--;
-}
-
-template<typename Type>
-void ListTwoway<Type>::pop()
-{
-    if (!tail) {
-        cout << "Lista jest pusta!" << endl;
-        return;
-    }
-
-
-    if (head == tail) {
-        delete tail;
-        head = tail = nullptr;
-    }
-    else {
-        Item<Type>* temp = tail;
-        tail = tail->prev;
-        tail->next = nullptr;
-        delete temp;
-    }
-
-
-    size--;
-}
-
-template<typename Type>
-void ListTwoway<Type>::clear()
-{
-    Item<Type>* current = head;
-    while (current != nullptr) {
-        Item<Type>* temp = current;
-        current = current->next;
-        delete temp;
-    }
-    head = tail = nullptr;
-    size = 0;
-}
-
-
-
-#endif
+#endif // LIST_TWOWAY_H
