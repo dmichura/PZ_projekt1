d31471cd6e377cc95416db6862319a5f0e4ae32d
dmichura <dejwidcode@gmail.com>
2025-10-26 02:57:55 +0200

    Added comments for doxygen

--- ListTwowayFactory.h
+++ ListTwowayFactory.h
@@ -1,14 +1,39 @@
+/**
+ * @file ListTwowayFactory.h
+ * @brief Definicja klasy szablonowej ListTwowayFactory — fabryka tworząca obiekty ListTwoway.
+ * 
+ * Klasa ListTwowayFactory umożliwia wygodne tworzenie nowych instancji listy dwukierunkowej
+ * bez bezpośredniego wywoływania konstruktora.
+ * 
+ * @tparam Type Typ danych przechowywanych w liście.
+ * @author dmichura
+ * @date 2025-10-25
+ */
+
 #ifndef LIST_TWOWAY_FACTORY_H
 #define LIST_TWOWAY_FACTORY_H
 
-#include "ListTwoway.h"
+#include "ListTwoway.cpp"
 
+/**
+ * @class ListTwowayFactory
+ * @brief Klasa szablonowa implementująca wzorzec fabryki dla listy dwukierunkowej.
+ * 
+ * Zawiera statyczną metodę `createList()`, która tworzy i zwraca wskaźnik
+ * na nową instancję klasy ListTwoway<Type>.
+ *
+ * @tparam Type Typ danych przechowywanych w liście.
+ */
 template<typename Type>
 class ListTwowayFactory {
-    public:
-        static ListTwoway<Type>* createList() {
-            return new ListTwoway<Type>();
-        }
+public:
+    /**
+     * @brief Tworzy nową instancję listy dwukierunkowej.
+     * @return Wskaźnik na nowo utworzoną listę ListTwoway<Type>.
+     */
+    static ListTwoway<Type>* createList() {
+        return new ListTwoway<Type>();
+    }
 };
 
-#endif
+#endif // LIST_TWOWAY_FACTORY_H
