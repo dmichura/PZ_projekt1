b600831781c10c8bd8b22a45201d96ba02bad979
dmichura <dejwidcode@gmail.com>
2025-10-26 02:56:53 +0200

    Added comments for doxygen

--- ListIterator.h
+++ ListIterator.h
@@ -1,73 +1,67 @@
+/**
+ * @file ListIterator.h
+ * @brief Definicja klasy szablonowej iterującej po elementach listy dwukierunkowej.
+ * @tparam Type Typ danych przechowywanych w liście.
+ * @author dmichura
+ * @date 2025-10-25
+ * Klasa ListIterator pozwala na przechodzenie po liście w obu kierunkach,
+ * wypisywanie elementów oraz resetowanie pozycji iteratora.
+ * Współpracuje z klasą ListTwoway.
+ */
+
 #ifndef LIST_ITERATOR_H
 #define LIST_ITERATOR_H
 
 #include "ListTwoway.h"
 
+/**
+ * @class ListIterator
+ * @brief Klasa implementująca iterator dla listy dwukierunkowej.
+ * 
+ * Klasa umożliwia iterację po elementach listy w przód i w tył.
+ * Wykorzystuje wskaźnik do listy (`ListTwoway<Type>`) oraz bieżący element (`Item<Type>*`).
+ * 
+ * @tparam Type Typ danych przechowywanych w liście.
+ */
 template<typename Type>
 class ListIterator {
-    private:
-        Item<Type>* current;  // Bieżący element w iteracji
-        ListTwoway<Type>* list;
-    public:
-        ListIterator(ListTwoway<Type>* list);
-        bool hasNext() const;
-        Type next();
-        void reset();
-        void printAll() const;
-        void printAllReverse() const;
+private:
+    Item<Type>* current;        ///< Wskaźnik na aktualny element listy.
+    ListTwoway<Type>* list;     ///< Wskaźnik na listę, po której iterator się porusza.
+
+public:
+    /**
+     * @brief Konstruktor klasy ListIterator.
+     * @param list Wskaźnik na listę, po której iterator ma się poruszać.
+     */
+    ListIterator(ListTwoway<Type>* list);
+
+    /**
+     * @brief Sprawdza, czy istnieje następny element w liście.
+     * @return true jeśli istnieje następny element, false w przeciwnym razie.
+     */
+    bool hasNext() const;
+
+    /**
+     * @brief Zwraca wartość następnego elementu i przesuwa iterator.
+     * @return Wartość elementu typu Type.
+     */
+    Type next();
+
+    /**
+     * @brief Resetuje iterator do początku listy.
+     */
+    void reset();
+
+    /**
+     * @brief Wypisuje wszystkie elementy listy w kolejności od początku do końca.
+     */
+    void printAll() const;
+
+    /**
+     * @brief Wypisuje wszystkie elementy listy w kolejności od końca do początku.
+     */
+    void printAllReverse() const;
 };
 
-template<typename Type>
-ListIterator<Type>::ListIterator(ListTwoway<Type>* list)
-{
-    this->list = list;
-    current = list->getHead();
-}
-
-template<typename Type>
-bool ListIterator<Type>::hasNext() const 
-{
-    return current != nullptr;
-}
-
-template<typename Type>
-Type ListIterator<Type>::next()
-{
-    if (current == nullptr) {
-        return false;
-    }
-    Type data = current->data;
-    current = current->next;  
-    return data;
-}
-
-template<typename Type>
-void ListIterator<Type>::reset()
-{
-    current = list->getHead();
-}
-
-template<typename Type>
-void ListIterator<Type>::printAll() const {
-    Item<Type>* currentItem = list->getHead();
-    cout << "{ ";
-    while (currentItem) {
-        cout << currentItem->data << " ";
-        currentItem = currentItem->next;
-    }
-    cout << "}" << endl;
-}
-
-template<typename Type>
-void ListIterator<Type>::printAllReverse() const {
-    Item<Type>* current = list->getTail();
-    
-    cout << "{ ";
-    while (current != nullptr) {
-        cout << current->data << " ";
-        current = current->prev;
-    }
-    cout << "}" << endl;
-}
-
-#endif
+#endif // LIST_ITERATOR_H
